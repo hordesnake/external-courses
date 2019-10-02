ca3ff7e8214fe06e8d0de77d6050faca6e637b10
hordesnake <dlyakovl@gmail.com>
2019-10-02 03:44:18 +0300

    fixed task-06

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -1,12 +1,12 @@
 'use strict';
 
-function checkOneCheck(number) {
+function checkPrime(number) {
   if (number > 1000) return "Данные неверны";
-  if (number === 0 || number === 1) return number + " принадлежит к классу нейтральных чисел";
+  if (number === 0 || number === 1) return "Число " + number + " принадлежит к классу нейтральных чисел";
   for (let i = 2; i < number; i++) {
-    if (!(number % i)) return number + " - составное число";
+    if (!(number % i)) return "Число " + number + " - составное число";
   }
-  return number + " - простое число";
+  return "Число " + number + " - простое число";
 };
 
-module.exports = checkOneCheck;
+module.exports = checkPrime;
