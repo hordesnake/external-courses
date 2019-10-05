7ea793ad2be637ff75f7cb04fbda54fbf281392a
hordesnake <dlyakovl@gmail.com>
2019-10-05 22:47:05 +0300

    added task 1-3

---
+++ src/ex4_js-objects-part2/task-03.js
@@ -0,0 +1,9 @@
+    'use strict';
+    let newStr = "     Max is a good boy     ";
+
+    function ReturnStrWithoutSideSpaces(str) {
+        newStr.trim();
+        return newStr;
+    };
+
+    module.exports = ReturnStrWithoutSideSpaces;
