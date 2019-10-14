e8fdd287f645b0c2b5d1da31055d4f820345a013
hordesnake <dlyakovl@gmail.com>
2019-10-14 10:14:36 +0300

    fixed delete console.log and comments

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -2,14 +2,12 @@
 
 function showType(retArr) {
   if (typeof retArr === 'string') {
-    console.log('Тип переменной: String');
-    return 'string'; // String return func
-  } else if (typeof retArr === 'number') {
-    console.log('Тип переменной: Number');
-    return 'number'; //Number return to end func
+    return 'string';
   }
-  console.log('Undefined');
+  if (typeof retArr === 'number' && !Number.isNaN(retArr)) {
+    return 'number';
+  }
   return undefined;
-
 }
+
 module.exports = showType;
