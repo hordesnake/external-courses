b295a1ed745f6e1e4334828b19f9d3ae9e6753ef
hordesnake <dlyakovl@gmail.com>
2019-10-14 10:48:11 +0300

    fixed

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,21 +1,12 @@
 'use strict';
 
-const obj = {
-  a: 1,
-  b: 2
-};
-const testObj = {
-  c: 3
-};
-
-function returnValueProtoProp(property, obj)
-{
-  for (let key in obj) {
-    if (!(obj.hasOwnProperty(key)) && key === property) {
-      return obj[key];
-    }
-  }
-  return undefined;
+function returnValueProtoProp(property, obj) {
+  for (var key in obj){
+		if(!obj.hasOwnProperty(key) && key === property){
+			return obj[key];
+		}
+	}
+	return undefined;
 }
 
 module.exports = returnValueProtoProp;
